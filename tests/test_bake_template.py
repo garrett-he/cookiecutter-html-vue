d08b8d305aa4d0f50f5264f990d65c247e47b715
Garrett HE <garrett.he@outlook.com>
2023-10-08 11:10:19 +0800

    Template: Add configuration file for package

--- tests/test_bake_template.py
+++ tests/test_bake_template.py
@@ -1,3 +1,4 @@
+import json
 import random
 from glob import glob
 
@@ -26,11 +27,15 @@ def generate_context() -> dict:
         'project_name': f'{chance.word().capitalize()} {chance.word().capitalize()}',
         'project_slug': f'{chance.word().lower()}-{chance.word().lower()}',
         'project_description': chance.sentence(),
+        'project_version': f'{random.randint(0, 10)}.{random.randint(0, 10)}.{random.randint(0, 10)}',
+        'project_private': chance.pickone(['true', 'false']),
+        'project_keywords': f'{chance.word()},{chance.word()},{chance.word()}',
         'author_name': chance.name(),
         'author_email': chance.email(),
         'license_id': chance.pickone([key for key in license_stubs.keys()]),
         'license_fullname': f'{chance.name()} <{chance.email()}>',
-        'license_year': str(random.randint(2000, 2023))
+        'license_year': str(random.randint(2000, 2023)),
+        'github_path': f'{chance.word()}/{chance.word()}-{chance.word()}'.lower()
     }
 
 
@@ -76,3 +81,23 @@ def test_bake_license(cookies: Cookies):
     readme = result.project_path.joinpath('README.md').read_text(encoding='utf-8')
     assert 'This is free and unencumbered software released into the public domain' in readme
     assert 'see [UNLICENSE](./UNLICENSE)' in readme
+
+
+def test_bake_package_json(cookies: Cookies):
+    context = generate_context()
+    result = cookies.bake(extra_context=context)
+    assert not result.exception
+
+    with result.project_path.joinpath('package.json').open('r', encoding='utf-8') as fp:
+        package_json = json.load(fp)
+
+    assert package_json['name'] == context['project_slug']
+    assert package_json['version'] == context['project_version']
+    assert package_json['description'] == context['project_description']
+    assert package_json['private'] == (context['project_private'] == 'true')
+    assert package_json['repository']['url'] == f'git+https://github.com/{context["github_path"]}.git'
+    assert package_json['keywords'] == context['project_keywords'].split(',')
+    assert package_json['author'] == f'{context["author_name"]} <{context["author_email"]}>'
+    assert package_json['license'] == context['license_id']
+    assert package_json['bugs']['url'] == f'https://github.com/{context["github_path"]}/issues'
+    assert package_json['homepage'] == f'https://github.com/{context["github_path"]}#readme'
